8aa63769871cc0722d6acecad3203528fa35ed0c
JsBok <bjs914@naver.com>
2021-03-15 22:56:37 +0900

    remove z_210309_SPStep08

--- z_210309_SPStep08/src/exception/DuplicateMemberException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class DuplicateMemberException extends RuntimeException{
-
-	//에러메시지를 출력하기 위한 메소드 선언
-	public DuplicateMemberException(String message) {
-		super(message);
-	}
-}
