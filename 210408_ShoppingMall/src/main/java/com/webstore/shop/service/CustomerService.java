9dec18aeefdfb70986fac2a6220215e16bc6131e
bjs914 <bjs914@naver.com>
2021-04-15 00:22:24 +0900

    add ShoppingMall

---
+++ 210408_ShoppingMall/src/main/java/com/webstore/shop/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.webstore.shop.service;
+
+import java.util.List;
+
+import com.webstore.shop.domain.Customer;
+
+public interface CustomerService {
+	List<Customer> getAllCustomers();
+}
